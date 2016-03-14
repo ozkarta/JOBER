fcb1182d7ab6f938198d5aa64d18937f3864ebb4
ozkarta <ozbegi1@gmail.com>
2016-03-14 15:47:13 +0400

    new
    theozkart

---
+++ JOBER/app.js
@@ -0,0 +1,160 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
+
+var app = express();
+
+//_______________REQUIRES__________________
+var config=require('./config/server')();
+var session=require('express-session');
+var expressLayouts=require('express-ejs-layouts');
+var expressPartials=require('express-partials');
+var fs=require('fs');
+var xmlObject=require('xml2js');
+var multer = require('multer');
+var utf8=require('utf8');
+var sql=require('mssql');
+var passport=require('passport');
+var flash=require('connect-flash');
+//-------------------CUSTOM-----------------
+var defaultRouting=require('./routes/defaultRouting');
+var db=require('./models/dbConnector.js');
+var smaConfigParser=require('./models/smaConfigParser');
+var sma=require('./models/smaLocals.js');
+
+
+var dbConnector=new db.dbConnector(sql);
+var smaLocals=new sma.smaLocals(dbConnector)
+var configReader=new smaConfigParser.configReader(sma,app,smaLocals,fs,dbConnector,xmlObject,utf8);
+var registerRoutings=new defaultRouting.registerRoutings(sma,smaLocals,passport);
+
+
+//__________________V2________________________
+
+var smaConfigParserV2=require('./models/smaConfigParserV2.JS');
+var usersObject=require('./models/usersObject.JS');
+var defaultRoutingV2=require('./routes/defaultRoutingV2.JS');
+
+var users=new usersObject.users();
+var configReaderV2=new smaConfigParserV2.configReader(usersObject,users,dbConnector);
+var registerRoutingsV2=new defaultRoutingV2.registerRoutings(app,usersObject,users,dbConnector)
+
+
+
+
+
+
+
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.use(expressPartials())
+app.use(session({secret:'ozkart',cookie:{maxAge:10000}}))  //  600 seconds  remaining from   first creation
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+//app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use('/', routes);
+//app.use('/users', users);
+
+// catch 404 and forward to error handler
+/*app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+*/
+
+app.listen(config.port, function(){
+    console.log('Express server listening on port ' + config.port);
+});
+
+
+//_________________________________________________________________
+function main(){
+  console.dir(sma)
+  configReader.parseConfig(function(){
+    console.log('triing to register routes .....')
+    registerRoutings.register(app,smaLocals,function(){
+      setLayout(smaLocals.activeUser.layoutName);
+      console.log('trying to set layout ')
+      //require('./server_scripts/authentication.js')(passport,sma,smaLocals);
+    });
+  });
+
+  
+  
+};
+
+function mainV2(){
+  configReaderV2.parseConfig(function(){
+    console.log('_________________________________________________________________')
+    console.log('config Parsing is complete!!!')
+    registerRoutingsV2.register(app,usersObject,users,dbConnector,function(){
+        console.log('routings registration was complete !!!');
+        //console.log(users.default_layout_name);
+        setLayoutV2(users.default_layout_name);
+    });
+  });
+
+  
+};
+
+
+function setLayout(layoutName){
+  app.set('layout','layouts/'+layoutName);
+  console.log('this was layout ------>'+layoutName)
+}
+function setLayoutV2(layoutName){
+  app.set('layout','layouts/'+layoutName);
+  console.log('this was layout ------>'+layoutName)
+}
+
+mainV2(function(){
+  console.log('______server was compiled succesfully_______')
+});
+
+
+
+
+//_________________________________________________________________
+
+//module.exports = app;
---
+++ JOBER/models/dbConnector.js
@@ -0,0 +1,278 @@
+var dbConnector=function(sql){
+	this.sql=sql;
+	this.connectionJson={
+		user:'node.js',
+		password:'12qwert12',
+		server:'78.139.172.254',
+		database:'smaDataBase',
+		options: {
+        	encrypt: true // Use this if you're on Windows Azure 
+    	}
+	};
+
+//Data Source=OZKART-PC;Initial Catalog=smaDataBase;Integrated Security=True
+
+
+	dbConnector.prototype.getLanguageList=function(){
+
+			var connection = new this.sql.Connection(this.connectionJson, function(err) {
+		    // ... error checks 
+		    //console.log(err.toString());
+		    // Query 
+		    
+		    var request = new sql.Request(connection); // or: var request = connection.request(); 
+		    request.query('select * from languages', function(err, recordset) {
+		        // ... error checks 
+		        //console.log(err.toString());
+		        
+		        //console.log('value from db is '+recordset[0]['languageName']);
+		    });
+		    
+		 
+			});
+	}
+	dbConnector.prototype.getLanguageGUID=function(languageSysName,callback){
+			var connection = new this.sql.Connection(this.connectionJson, function(err) {
+		    	var request=new sql.Request(connection);
+				request.input('languageName', sql.VarChar, languageSysName);
+			    //request.output('output_parameter', sql.VarChar(50));
+			    request.execute('getLanguageGUID', function(err, recordsets, returnValue) {
+			        // ... error checks 
+			        //console.log('began  db  '+languageSysName.toString());
+				        if( recordsets != undefined ){
+				        	if( recordsets[0] != undefined ){
+					        		if( recordsets[0][0] != undefined ){
+					        		 //console.log(recordsets[0][0]);
+							        //console.log(languageSysName+' __ '+recordsets[0][0].languageGUID);
+							        callback(recordsets[0][0].languageGUID,languageSysName);
+							       // return recordsets[0][0].languageGUID;
+							    }else{
+						    		console.log('broken  party!!!');
+						    		callback(undefined,undefined);
+						    	}
+				        	}else{
+						    		console.log('broken  party!!!');
+						    		callback(undefined,undefined);
+						    	}							       
+				    	}else{
+					    		console.log('broken  party!!!');
+					    		callback(undefined,undefined);
+					    	}
+
+	    			});
+		 
+			});
+
+ 
+	}
+
+	dbConnector.prototype.getLnaguageDisplayName=function(languageSysName,callback){
+		if(languageSysName==undefined){
+			callback(undefined,undefined);
+		}
+		console.log('Requested the  Display Name');
+		var connection = new this.sql.Connection(this.connectionJson, function(err) {
+			    	var request=new sql.Request(connection);
+					request.input('languageName', sql.VarChar, languageSysName);
+				    //request.output('output_parameter', sql.VarChar(50));
+				    request.execute('getLanguageDisplayName', function(err, recordsets, returnValue) {
+				        // ... error checks 
+				       // console.log('began  db  '+languageSysName.toString());
+					        if( recordsets != undefined ){
+					        	if( recordsets[0] != undefined ){
+						        		if( recordsets[0][0] != undefined ){
+						        		 //console.log(recordsets[0][0]);
+								       // console.log(languageSysName+' __ '+recordsets[0][0].languageDisplayName);
+								        callback(recordsets[0][0].languageDisplayName,languageSysName);
+								       // return recordsets[0][0].languageDisplayName;
+								    }else{
+							    		console.log('broken  party!!!');
+							    		callback(undefined,undefined);
+							    	}
+					        	}else{
+							    		console.log('broken  party!!!');
+							    		callback(undefined,undefined);
+							    	}							       
+					    	}else{
+						    		console.log('broken  party!!!');
+						    		callback(undefined,undefined);
+						    	}
+
+		    			});
+			 
+				});
+
+		}
+
+
+	dbConnector.prototype.getVariableTranslated=function(languageGUID,variableName,callback){
+		var connection=new this.sql.Connection(this.connectionJson,function(error){
+			var request=new sql.Request(connection);
+			request.input('languageGUID',sql.VarChar,languageGUID);
+			request.input('variableName',sql.VarChar,variableName);
+			request.execute('getTranslatedVariableValue',function(err,recordsets,returnValue){
+				//console.log('translation..............');
+				//console.log('getTranslatedVariableValue '+languageGUID+'  ,  '+variableName)
+
+				if(recordsets!= undefined){
+					if(recordsets[0]!=undefined){
+						if(recordsets[0][0]!=undefined){
+							//console.log(recordsets[0][0].value);
+							callback(recordsets[0][0].value.toString());
+						}else{
+							callback(undefined);
+						}
+					}else{
+						callback(undefined);
+					}
+				}else{
+						callback(undefined);
+					}
+				
+			});
+		});
+	}
+
+	dbConnector.prototype.newguid=function(callback){
+		var connection=new this.sql.Connection(this.connectionJson,function(error){
+			var request=new sql.Request(connection);
+			request.query('select newid() as guid',function(error,recordsets){
+				//console.log('___guid____request')
+				//console.dir(recordsets[0].guid.toString());
+				if(recordsets!= undefined){
+					if(recordsets[0]!=undefined){
+						if(recordsets[0].guid!=undefined){
+							//console.log(recordsets[0][0].value);
+							callback(recordsets[0].guid.toString());
+						}else{
+							callback(undefined);
+						}
+					}else{
+						callback(undefined);
+					}
+				}else{
+						callback(undefined);
+					}
+			});
+		});
+	}
+	
+	dbConnector.prototype.executeReadFromStringQuery=function(query,callback){
+		console.log('query  to execute  is  '+ query)
+		var connection=new this.sql.Connection(this.connectionJson,function(error){
+			var request=new sql.Request(connection);
+			request.query(query,function(error,recordsets){
+				
+				callback(recordsets);
+			});
+		});
+	}
+
+	dbConnector.prototype.signUp=function(languageGUID,levelGUID,email,emailCinfirmed,passwordHash,salt,phoneNumber,phoneNumberConfirmed,accessFailedCount,userName,firstName,lastName,passportID,callback){
+		var connection=new this.sql.Connection(this.connectionJson,function(error){
+			var request=new sql.Request(connection);
+			request.input('languageGUID',sql.VarChar,languageGUID);
+			request.input('levelGUID',sql.VarChar,levelGUID);
+			request.input('email',sql.VarChar,email);
+			request.input('emailConfirmed',sql.VarChar,emailCinfirmed);
+			request.input('passwordHash',sql.VarChar,passwordHash);
+			request.input('salt',sql.VarChar,salt);
+			request.input('phoneNumber',sql.VarChar,phoneNumber);
+			request.input('phoneNumberConfirmed',sql.VarChar,phoneNumberConfirmed);
+			request.input('accessFailedCount',sql.Int,accessFailedCount);
+			request.input('userName',sql.VarChar,userName);
+			request.input('firstName',sql.VarChar,firstName);
+			request.input('lastName',sql.VarChar,lastName);
+			request.input('passportID',sql.VarChar,passportID);
+
+
+			request.execute('registerUser',function(err,recordsets,returnValue){
+
+				console.log('error is ----')
+				console.dir(err);
+				console.log('calue is -----')
+				console.dir(recordsets[0][0]);
+				if(recordsets!== undefined){
+					if(recordsets[0]!==undefined){
+						if(recordsets[0][0]!==undefined){
+							callback(recordsets[0][0]);
+						}else{
+							callback(undefined);
+						}
+					}else{
+						callback(undefined);
+					}
+				}else{
+						callback(undefined);
+					}
+				
+			});
+		});
+
+
+
+
+		//callback('client');
+	}
+
+	dbConnector.prototype.signIn=function(email,password,callback){
+		//callback('client');
+		var connection=new this.sql.Connection(this.connectionJson,function(error){
+			var request=new sql.Request(connection);
+			request.input('email',sql.VarChar,email);
+			request.input('passwordHash',sql.VarChar,password);
+			
+
+
+			request.execute('signIn',function(err,recordsets,returnValue){
+
+				console.log('error is ----')
+				console.dir(err);
+				console.log('calue is -----')
+				console.dir(recordsets[0][0]);
+				if(recordsets!== undefined){
+					if(recordsets[0]!==undefined){
+						if(recordsets[0][0]!==undefined){
+							callback(recordsets[0][0]);
+						}else{
+							callback(undefined);
+						}
+					}else{
+						callback(undefined);
+					}
+				}else{
+						callback(undefined);
+					}
+				
+			});
+		});
+
+
+	}
+
+	dbConnector.prototype.getAccessLevelGUID=function(levelName,callback){
+		var connection=new this.sql.Connection(this.connectionJson,function(error){
+			var request=new sql.Request(connection);
+			request.query('select levelGUID from accessLevels where levelName=\''+levelName+'\''.toString(),function(error,recordsets){
+				if(recordsets!= undefined){
+					if(recordsets[0]!=undefined){
+						if(recordsets[0].levelGUID!=undefined){
+							callback(recordsets[0].levelGUID.toString());
+						}else{
+							callback(undefined);
+						}
+					}else{
+						callback(undefined);
+					}
+				}else{
+						callback(undefined);
+					}
+			});
+		});
+	}	
+};
+
+
+
+
+module.exports.dbConnector=dbConnector;
